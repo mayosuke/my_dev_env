018fbc00f7b88652ca2b9c953284e737d375eab5
mayosuke <mayo.suke@gmail.com>
2014-12-12 03:02:20 +0900

    Add user-config.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,10 @@
 apt-get update
 apt-get -y upgrade
 
+which vim || apt-get -y install vim
+which git || apt-get -y install git
+which curl || apt-get -y install curl
+which ruby || apt-get -y install ruby
+
+su -c "source /vagrant/user-config.sh" vagrant
+
---
+++ user-config.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "export EDITOR=vim" >> $HOME/.bashrc
+
